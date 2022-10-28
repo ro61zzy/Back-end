98649fa4c2a7d9dd0b18ae1d8c88c981de7228a7
Rose Karemeri <rosekaremeri@gmail.com>
2022-10-28 12:35:50 +0300

    update

--- Expressjs/index.js
+++ Expressjs/index.js
@@ -1,6 +1,18 @@
 var express = require('express')
 var app = express()
+
 app.get('/getUsers', (req, res) => {
   res.send('Welcome to express')
 })
+
+app.get('/getTrained', (req, res) => {
+  var giveTrainingObj = {
+    trainee: 'name',
+    game: 'boxing',
+    number: 4,
+  }
+
+  res.send(giveTrainingObj)
+})
+
 app.listen(4000)
