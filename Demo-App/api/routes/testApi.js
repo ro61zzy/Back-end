010e09de2bfba86ab823a610d0fb5a0cca2b1c7d
Rose Karemeri <rosekaremeri@gmail.com>
2022-11-09 14:41:37 +0300

    connection success

--- Demo-App/api/routes/testApi.js
+++ Demo-App/api/routes/testApi.js
@@ -1,7 +1,7 @@
 var express = require('express')
 var router = express.Router()
 
-var express = router.Router()
+//var express = router.Router()
 
 router.get('/', (req, res, next) => {
   res.send('API is working properly')
