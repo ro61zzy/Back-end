93a4e4f546a6cf24c7dd505440f8159ec03903c7
Rose Karemeri <rosekaremeri@gmail.com>
2022-11-09 13:59:19 +0300

    first router

--- Demo-App/api/routes/testApi.js
+++ Demo-App/api/routes/testApi.js
@@ -1,1 +1,10 @@
 var express = require('express')
+var router = express.Router()
+
+var express = router.Router()
+
+router.get('/', (req, res, next) => {
+  res.send('API is working properly')
+})
+
+module.exports = router
