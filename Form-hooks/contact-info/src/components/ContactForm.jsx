2589b147d4cc2407d026e5bb95714123a20c375a
Rose Karemeri <rosekaremeri@gmail.com>
2022-11-10 14:50:30 +0300

    useState

--- Form-hooks/contact-info/src/components/ContactForm.jsx
+++ Form-hooks/contact-info/src/components/ContactForm.jsx
@@ -1,20 +1,52 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function ContactForm() {
+  const [contactInfo, setContactInfo] = useState({
+    //this are the state properties
+    name: '',
+    email: '',
+    phonenumber: '',
+  })
+  //now create an onChange event listener to help react detect changes in the form
+  const handleChange = (event) => {
+    setContactInfo({ ...contactInfo, [event.target.name]: event.target.value })
+  }
+  const handleSubmit = (event) => {
+    // prevents the submit button from refreshing the page
+    event.preventDefault()
+    console.log(contactInfo)
+    setContactInfo({ name: ' ', email: ' ', phonenumber: ' ' })
+  }
+
   return (
     <>
-      <form>
+      <form onSubmit={handleSubmit}>
         <div>
           <h3>Contact Form</h3>
         </div>
         <div>
-          <input type='text' name='name' placeholder='Name' />
+          <input
+            type='text'
+            name='name'
+            placeholder='Name'
+            value={contactInfo.name}
+          />
         </div>
         <div>
-          <input type='email' name='email' placeholder='Email' />
+          <input
+            type='email'
+            name='email'
+            placeholder='Email'
+            value={contactInfo.email}
+          />
         </div>
         <div>
-          <input type='number' name='phonenumber' placeholder='Phone Number' />
+          <input
+            type='number'
+            name='phonenumber'
+            placeholder='Phone Number'
+            value={contactInfo.phonenumber}
+          />
         </div>
         <div>
           <button>Submit Contact</button>
